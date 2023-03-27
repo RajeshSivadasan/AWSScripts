993ab68309d6d4ff950ec32132b6f32e61da414b
Rajesh Sivadasan <36217458+RajeshSivadasan@users.noreply.github.com>
2023-03-28 05:29:28 +0530

    Update aws_start_instance.bat

--- aws_start_instance.bat
+++ aws_start_instance.bat
@@ -52,6 +52,7 @@ FOR /F "tokens=*" %%g IN ('%myCMD%') do (SET VAR=%%g)
 
 echo Public IP = %VAR%
 
+rem Start code block For Windows RDP
 set mysrvid=Administrator
 
 set mysrvpswd=YourPassword
@@ -63,5 +64,24 @@ cmdkey /generic:%VAR% /user:%mysrvid% /pass:%mysrvpswd%
 mstsc.exe /v:%VAR%
 
 cmdkey /delete:%VAR%
+
+rem End code block For Windows RDP
+
+
+rem for setting the config file host and ip to connect to linux using visual studio code
+( 
+echo Host ubuntu 
+echo HostName %VAR% 
+echo User ubuntu 
+echo IdentityFile C:\Users\RajeshSivadasan\.ssh\ubuntu.pem 
+echo[ 
+)> C:\Users\RajeshSivadasan\.ssh\config 
+echo Config file updated with IP address 
+REM in case of multiple hosts use the below (for 3 hosts) for merging into one
+REM type C:\Users\Admin\.ssh\config1 > C:\Users\Admin\.ssh\config 
+REM type C:\Users\Admin\.ssh\config2 >> C:\Users\Admin\.ssh\config 
+REM type C:\Users\Admin\.ssh\config3 >> C:\Users\Admin\.ssh\config 
+echo merged config files 
+PAUSE REM "D:\Program Files\Microsoft VS Code\Code.exe"
 
 rem exit
