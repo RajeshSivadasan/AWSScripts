814dd542e1dabde89cf3f4d9f7b6aa88379180d8
Rajesh Sivadasan <36217458+RajeshSivadasan@users.noreply.github.com>
2025-05-05 22:32:25 +0530

    Update aws_lambda_function_stop.py

--- aws_lambda_function_stop.py
+++ aws_lambda_function_stop.py
@@ -1,7 +1,7 @@
 import boto3
 client=boto3.client("ec2")
 
-def stop_ec2(event, context):
-    id = ['<instance id>']
+def lambda_handler(event, context):
+    id = ['i-008babb7cd3097249']
     client.stop_instances(InstanceIds=id)
     return("stop_ec2 done")
